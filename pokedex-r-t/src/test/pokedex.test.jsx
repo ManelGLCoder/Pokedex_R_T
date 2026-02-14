08da92735272426a42bdc18fa2c07b8829d1e8b5
ManelGLCoder <manelglcoder@gmail.com>
2026-02-14 16:23:20 +0100

    refactor basic tests, change fetch functions to utilities file, add todos tests

--- pokedex-r-t/src/test/pokedex.test.jsx
+++ pokedex-r-t/src/test/pokedex.test.jsx
@@ -2,7 +2,9 @@
 // import { json } from 'node:stream/consumers'
 // import { useState } from 'react'
 import { afterEach, describe, it, expect, vi} from 'vitest'
-import { getPokemonId, getPokemonDataById } from '../components/principal-sections/SectionAllPokemonData'
+import { 
+        fetchPokemonDataSimplified, fetchPokemonData , fetchAbility, fetchNameAbilityInLang
+     } from '../utilities/fetch-utilities'
 
 
 describe('PokemApi REQUEST FUNCTIONS', () =>{
@@ -10,54 +12,21 @@ describe('PokemApi REQUEST FUNCTIONS', () =>{
         vi.clearAllMocks()
     })
 
-    it('should getPokemonId be a function', ()=>{
-        expect(typeof getPokemonId).toBe('function')
+    it('should [fetchPokemonData] be a function', ()=>{
+        expect(typeof fetchPokemonData).toBe('function')
     })
 
-    it('should fetch pokemon id requested data from PokeApi', async ()=>{
-        global.fetch = vi.fn(() =>
-            Promise.resolve({
-            json: () => Promise.resolve(CHARIZARD_FORM_DATA),
-            }),
-        );
-        
-        const charizardId = await getPokemonId("Charizard")
-        // console.log(`Charizard ID: ${CHARIZARD_ID}, id obtained: ${charizardId}`)
-        expect(charizardId).toBe(CHARIZARD_ID)
-        expect(fetch).toHaveBeenCalledTimes(1);
-        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon-form/Charizard/')
-        
-        vi.clearAllMocks()
-
-        global.fetch = vi.fn(() =>
-            Promise.resolve({
-            json: () => Promise.resolve(PIKACHU_FORM_DATA),
-            }),
-        );
-        
-        const pikachuId = await getPokemonId("Pikachu")
-        // console.log(`Pikachu ID: ${PIKACHU_ID}, id obtained: ${pikachuId}`)
-        expect(pikachuId).toBe(PIKACHU_ID)
-        expect(fetch).toHaveBeenCalledTimes(1);
-        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon-form/Pikachu/')
-
-    })
-
-    it('should getPokemonDataById be a function', ()=>{
-        expect(typeof getPokemonDataById).toBe('function')
-    })
-
-    it('should fetch pokemon data by id from POKE-API',async ()=>{
+    it('should fetch pokemon data by id or name from POKE-API',async ()=>{
         global.fetch = vi.fn(() =>
             Promise.resolve({
             json: () => Promise.resolve(CHARIZARD_DATA),
             }),
-        );
+        )
         
-        const charizardData = await getPokemonDataById(CHARIZARD_ID)
+        const charizardData = await fetchPokemonData("Charizard")
         expect(charizardData).toEqual(CHARIZARD_DATA)
-        expect(fetch).toHaveBeenCalledTimes(1);
-        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/6/')
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/Charizard/')
 
         vi.clearAllMocks()
 
@@ -65,21 +34,126 @@ describe('PokemApi REQUEST FUNCTIONS', () =>{
             Promise.resolve({
             json: () => Promise.resolve(PIKACHU_DATA),
             }),
-        );
+        )
         
-        const pikachuData = await getPokemonDataById(PIKACHU_ID)
+        const pikachuData = await fetchPokemonData(PIKACHU_ID)
         expect(pikachuData).toEqual(PIKACHU_DATA)
-        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledTimes(1)
         expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/25/')
     })
 
-    it.todo('should get abilities from PokeAPI', async()=>{
+    it('should [fetchAbility] be a function', async ()=>{
+        expect(typeof fetchAbility).toBe('function')
     })
 
-    it.todo('should get sprites from PokeAPI', async()=>{
+    it.skip('should fetch pokemon abilities from PokeAPI', async()=>{
+        global.fetch = vi.fn(() =>
+            Promise.resolve({
+            json: () => Promise.resolve(BLAZE_ABILITY_DATA),
+            }),
+        )
+        
+        const blazeData = await fetchAbility(BLAZE_ABILITY_URL)
+        expect(blazeData).toEqual(BLAZE_ABILITY_DATA)
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/ability/66/')
+
+        vi.clearAllMocks()
+
+        global.fetch = vi.fn(() =>
+            Promise.resolve({
+            json: () => Promise.resolve(SOLAR_POWER_ABILITY_DATA),
+            }),
+        )
+        
+        const solarPowerData = await fetchAbility(SOLAR_POWER_ABILITY_URL)
+        expect(solarPowerData).toEqual(SOLAR_POWER_ABILITY_DATA)
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/ability/94/')
     })
 
-    it.todo('should get moves from PokeAPI', async()=>{
+    it('should [fetchNameAbilityInLang] be a function', async ()=>{
+        expect(typeof fetchNameAbilityInLang).toBe('function')
+    })
+
+    it('should fetch ability name in correct language from PokeAPI', async()=>{
+        global.fetch = vi.fn(() =>
+            Promise.resolve({
+            json: () => Promise.resolve(BLAZE_ABILITY_DATA),
+            }),
+        )
+        
+        const blazeData = await fetchAbility(BLAZE_ABILITY_URL)
+        expect(blazeData).toEqual(BLAZE_ABILITY_DATA)
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/ability/66/')
+
+        const blazeES = await fetchNameAbilityInLang(blazeData, "es")
+        expect(blazeES).toBe(BLAZE_ABILITY_ES)
+
+        vi.clearAllMocks()
+
+        global.fetch = vi.fn(() =>
+            Promise.resolve({
+            json: () => Promise.resolve(SOLAR_POWER_ABILITY_DATA),
+            }),
+        )
+        
+        const solarPowerData = await fetchAbility(SOLAR_POWER_ABILITY_URL)
+        expect(solarPowerData).toEqual(SOLAR_POWER_ABILITY_DATA)
+        expect(fetch).toHaveBeenCalledTimes(1)
+        expect(fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/ability/94/')
+
+        const solarPowerES = await fetchNameAbilityInLang(solarPowerData, "es")
+        expect(solarPowerES).toBe(SOLAR_POWER_ABILITY_ES)
+    })
+
+    it.todo('should fetch moves of specific group from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch data specific move from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch stats from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch stat in correct language from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch types from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch type in correct language from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch chain evolution from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch evolution data from PokeAPI', async()=>{
+    })
+
+    it.todo('should fetch simplified data(pokemonForm) from PokeAPI', async()=>{
+    })
+
+    it.todo('should get pokemon data needed for PokemonInfo section from PokeAPI', async()=>{
+    })
+
+    it.todo('should get pokemon data simplified needed for PokemonList section from PokeAPI', async()=>{
+    })
+
+    it.todo('should get sprites data from pokemonData', async()=>{
+    })
+
+    it.todo('should get moves by group data from pokemonData', async()=>{
+    })
+
+    it.todo('should get types data from pokemonData', async()=>{
+    })
+
+    it.todo('should get stats data from pokemonData', async()=>{
+    })
+
+    it.todo('should get evolution data from pokemonData', async()=>{
     })
 
 
---
+++ pokedex-r-t/src/utilities/fetch-utilities.js
@@ -0,0 +1,34 @@
+export async function fetchPokemonDataSimplified(pokemon){
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${pokemon}/`)
+        const data = await response.json()
+        return data
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function fetchPokemonData(pokemon){
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+        const data = await response.json()
+        return data
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function fetchAbility(abilityURL) {
+    try{
+        const response = await fetch(abilityURL)
+        const data = await response.json()
+        return data
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export async function fetchNameAbilityInLang(abilityData, language) {
+    const abilityInLang = abilityData.names.find((element) => {return element.language.name === language})
+    return  abilityInLang.name
+}
