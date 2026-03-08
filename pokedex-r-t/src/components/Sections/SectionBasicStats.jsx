a4207f647a1727e72b0a6f41018cc050f457309f
ManelGLCoder <manelglcoder@gmail.com>
2026-03-08 11:04:33 +0100

    fix stats error

--- pokedex-r-t/src/components/Sections/SectionBasicStats.jsx
+++ pokedex-r-t/src/components/Sections/SectionBasicStats.jsx
@@ -8,7 +8,7 @@ const SectionBasicStats = ({stats}) => {
         <section className="py-2">
             <span className="text-xl font-bold">Estadísticas base</span>
             <div className="flex flex-col">
-                <Stat name={"PS"} val={stats.ps} maxVal={stats.statMax}/>
+                <Stat name={"PS"} val={stats.hp} maxVal={stats.statMax}/>
                 <Stat name={"Ataque"} val={stats.attack} maxVal={stats.statMax}/>
                 <Stat name={"Defensa"} val={stats.defense} maxVal={stats.statMax}/>
                 <Stat name={"Velocidad"} val={stats.speed} maxVal={stats.statMax}/>
