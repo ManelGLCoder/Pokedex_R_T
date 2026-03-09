8169182f0b1c681d5f318b14fd3cec01c1aaf305
ManelGLCoder <manelglcoder@gmail.com>
2026-03-09 14:31:35 +0100

    Add coming soon to features will not be implemented in a near future

---
+++ pokedex-r-t/src/components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+const ComingSoon = () =>{
+    return(
+        <div className="flex flex-1 justify-center">
+            <img className="size-7" src="./src/assets/warning.svg" alt="Warning Icon" />
+            <span className="text-xl text-amber-200">Próximamente...</span>
+            <img className="size-7" src="./src/assets/warning.svg" alt="Warning Icon" />
+        </div>
+    )
+}
+
+export default ComingSoon
--- pokedex-r-t/src/components/Sections/SectionWeakness.jsx
+++ pokedex-r-t/src/components/Sections/SectionWeakness.jsx
@@ -1,13 +1,15 @@
 import PokemonType from "../elements/PokemonType"
+import ComingSoon from "../ComingSoon"
 
 const SectionWeakness = ({weakness}) =>{
     return(
         <section className="py-1">
             <span className="text-xl font-bold">Debilidades</span>
             <div className={`flex flex-wrap justify-normal gap-2 px-5 my-1`}>
-                {weakness && weakness.map((weak, i)=>{
+                <ComingSoon/>
+                {/* {weakness && weakness.map((weak, i)=>{
                     return (<PokemonType key={i} pokemonType={weak.type} strengthMultiplier={weak.multiplier} />)
-                })}
+                })} */}
             </div>
         </section>
     )
